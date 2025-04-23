4ec99c0fdfd2356d99196efa49a6b3adc339e870
Yeung <yohimhim@iastate.edu>
2025-04-23 13:03:23 -0500

    modified simple sensor socket

--- python_gui/3-simple mock sensor client server starter code/simple-sensor-Socket-or-UART-client.py
+++ python_gui/3-simple mock sensor client server starter code/simple-sensor-Socket-or-UART-client.py
@@ -11,8 +11,9 @@ import time # Time library
 # Socket library:  https://realpython.com/python-sockets/  
 # See: Background, Socket API Overview, and TCP Sockets  
 import socket   
-
 import os  # import function for finding absolute path to this python script
+
+import keyboard # remember to pip install keyboard
 
 # A little python magic to make it more convient for you to adjust where you want the data file to live
 # Link for more info: https://towardsthecloud.com/get-relative-path-python 
@@ -27,50 +28,49 @@ filename = 'sensor-scan.txt' # Name of file you want to store sensor data from y
 # UART END
 
 # TCP Socket BEGIN (See Echo Client example): https://realpython.com/python-sockets/#echo-client-and-server
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = "192.168.1.1"  # The server's hostname or IP address
+PORT = 288        # The port used by the server
 cybot_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
 cybot_socket.connect((HOST, PORT))   # Connect to the socket  (Note: Server must first be running)
                       
 cybot = cybot_socket.makefile("rbw", buffering=0)  # makefile creates a file object out of a socket:  https://pythontic.com/modules/socket/makefile
 # TCP Socket END
 
-# Send some text: Either 1) Choose "Hello" or 2) have the user enter text to send
-send_message = "Hello\n"                            # 1) Hard code message to "Hello", or
-# send_message = input("Enter a message:") + '\n'   # 2) Have user enter text
+print("Press a key to send command to CyBot (press 'q' to quit)...");
 
-cybot.write(send_message.encode()) # Convert String to bytes (i.e., encode), and send data to the server
+while True:
+        event = keyboard.read_event()
 
-print("Sent to server: " + send_message) 
+        if event.event_type == keyboard.KEY_DOWN: #react once the key is pressed - not when it's released
+                char = event.name
 
-# Send messges to server until user sends "quit"
-while send_message != 'quit\n':
+                if len(char) == 1:
+                        send_message = char + '\n'
+                        print(f"SendingL {char}")
+                        cybot.write(send_message.encode())
 
-        # Check if a sensor scan command has been sent
-        if (send_message == "M\n") or (send_message == "m\n"):
+                        if char.lower() == 'q':
+                                break
 
-                print("Requested Sensor scan from Cybot:\n")
-                rx_message = bytearray(1) # Initialize a byte array
+                        if char.lower() == 'm':
+                                print("Requested Sensor scan from Cybot:\n")
+                                rx_message = bytearray(1) # Initialize a byte array
 
-                # Create or overwrite existing sensor scan data file
-                file_object = open(full_path + filename,'w') # Open the file: file_object is just a variable for the file "handler" returned by open()
+                                # Create or overwrite existing sensor scan data file
+                                file_object = open(full_path + filename,'w') # Open the file: file_object is just a variable for the file "handler" returned by open()
 
-                while (rx_message.decode() != "END\n"): # Collect sensor data until "END" recieved
-                        rx_message = cybot.readline()   # Wait for sensor response, readline expects message to end with "\n"
-                        file_object.write(rx_message.decode())  # Write a line of sensor data to the file
-                        print(rx_message.decode()) # Convert message from bytes to String (i.e., decode), then print
+                                while (rx_message.decode() != "END\n"): # Collect sensor data until "END" recieved
+                                        rx_message = cybot.readline()   # Wait for sensor response, readline expects message to end with "\n"
+                                        file_object.write(rx_message.decode())  # Write a line of sensor data to the file
+                                        print(rx_message.decode()) # Convert message from bytes to String (i.e., decode), then print
 
-                file_object.close() # Important to close file once you are done with it!!                
+                                file_object.close() # Important to close file once you are done with it!!  
+                        else:
+                                print("Waiting for server reply\n")
+                                rx_message = cybot.readline()
+                                print("Got a message from serer: " + rx_message.decode() + "\n")
 
-        else:                
-                print("Waiting for server reply\n")
-                rx_message = cybot.readline()      # Wait for a message, readline expects message to end with "\n"
-                print("Got a message from server: " + rx_message.decode() + "\n") # Convert message from bytes to String (i.e., decode)
-        
-        send_message = input("Enter a message (enter quit to exit):") + '\n' # Enter next message to send to server
-        cybot.write(send_message.encode()) # Convert String to bytes (i.e., encode), and send data to the server
-        
-print("Client exiting, and closing file descriptor, and/or network socket\n")
-time.sleep(2) # Sleep for 2 seconds
-cybot.close() # Close file object associated with the socket or UART
-#cybot_socket.close()  # Close the socket (NOTE: comment out if using UART interface, only use for network socket option)
+print("Client exiting, closing connections...")
+time.sleep(2)
+cybot.close()
+cybot_socket.close()
