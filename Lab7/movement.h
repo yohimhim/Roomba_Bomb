a830493b380f5fa85b5be58eceb044aac5225259
Cooper Hoy <144978421+34coopatroopa@users.noreply.github.com>
2025-04-29 15:13:17 -0500

    Add files via upload

---
+++ Lab7/movement.h
@@ -0,0 +1,31 @@
+/*
+ * movement.h
+ *
+ *  Created on: Feb 7, 2025
+ *      Author: aheiar
+ */
+
+#include "open_interface.h"
+
+#ifndef MOVEMENT_H_
+#define MOVEMENT_H_
+
+/**
+ * Manual Movement
+ */
+void manual_move_forward(void);
+void manual_move_backward(void);
+void manual_turn_left(void);
+void manual_turn_right(void);
+void manual_stop(void);
+
+/**
+ * Autonomous Movement
+ */
+void move_forward (oi_t *sensor_data, double distance_mm);
+void move_right(oi_t *sensor_data, double angle);
+void move_left(oi_t *sensor_data, double angle);
+void move (oi_t *sensor_data, double distance_mm, double wheel1, double wheel2);
+void move_backward (oi_t *sensor_data, double distance_mm);
+int get_cliff(oi_t *sensor);
+#endif /* MOVEMENT_H_ */
