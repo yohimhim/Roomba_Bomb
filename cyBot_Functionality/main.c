23f71226ad7c4b696f9484f1c8551525d429b48e
Marcus Yeung <101527726+yohimhim@users.noreply.github.com>
2025-05-05 00:12:55 -0500

    Adding C files

---
+++ cyBot_Functionality/main.c
@@ -0,0 +1,288 @@
+#include <stdint.h>
+#include "uart-interrupt.h"
+#include "Timer.h"
+#include "lcd.h"
+#include "movement.h"
+#include "open_interface.h"
+#include "cyBot_uart.h"
+#include "cyBot_Scan.h"
+#include "uart-interrupt.h"
+#include "stdbool.h"
+#include "main.h"
+#include "scan.h"
+#include "adc.h"
+
+
+
+#warning "Possible unimplemented functions"
+
+enum driver_state_t state = MANUAL;
+
+
+    void main() {
+    oi_t *sensor_data = oi_alloc(); // do this only once at start of main()
+    oi_init(sensor_data); // do this only once at start of main()
+    timer_init();
+    lcd_init();
+    uart_interrupt_init();
+
+    adc_init();
+    cyBot_uart_init();
+    cyBOT_init_Scan(0b0111);
+
+    char my_data;       // Variable to get bytes from Client
+//    char command[100];  // Buffer to store command from Client
+//    int index = 0;      // Index position within the command buffer
+
+    //cyBOT_SERVO_Cal();
+    right_calibration_value = 253750;
+    left_calibration_value = 1288000;
+    double sum = 0;
+    double distance = 0;
+    cyBOT_Scan_t scan;
+
+    float firstScan;
+    float secondScan;
+
+    int atBomb = 0;
+
+    oi_setWheels(0,0);
+
+
+
+    //star wars
+    //
+        unsigned char notes[9] = {
+               67, 67, 67, 63, 70, 67, 63, 70, 67
+           };
+
+           // Durations (1/64 second units); 32 = 0.5 sec, 16 = 0.25 sec
+           unsigned char durations[9] = {
+               32, 32, 32, 24, 8, 32, 24, 8, 48
+           };
+
+           // Load song to slot 0
+           oi_loadSong(0, 9, notes, durations);
+
+
+
+
+    while(1)
+    {
+
+
+
+
+        oi_update(sensor_data);
+
+
+        //flag_monitor(sensor_data);
+        if (state == AUTO){
+
+            while (state == AUTO && !atBomb){
+                //AUTO MODE DRIVING
+                lcd_printf("we are in auto");
+
+
+                //Return if it reaches the object
+
+
+                sum = 0;
+                distance = 0;
+                     flag_monitor(sensor_data);
+                     oi_update(sensor_data);
+
+
+
+                     float angleToObject1 =  run_autonomous_scan(sensor_data,&scan); //Angle from first scan
+                     firstScan = get_largest_width();
+                     flag_monitor(sensor_data);
+
+                     //If the scan is cancelled, we don't want it to calculate angles
+                     if(angleToObject1 == -1){
+                         cancel_scan_flag = 0;
+                         break;
+                     }
+
+                     //Turn 180 degrees
+                     oi_setWheels(-100, 100);
+                     timer_waitMillis(3200);
+                     oi_setWheels(0, 0);
+
+                     float angleToObject2 =  run_autonomous_scan(sensor_data,&scan); //Angle from second scan
+                     secondScan = get_largest_width();
+                     flag_monitor(sensor_data);
+
+                     //If the scan is cancelled, we don't want it to calculate angles
+                     if(angleToObject2 == -1){
+                        cancel_scan_flag = 0;
+                      break;
+                    }
+
+                     lcd_printf("first: %lf\nsecond: %lf",firstScan, secondScan);
+
+                     //compare the scans
+                     if(firstScan >= secondScan){
+                          //Turn 180 degrees
+                        oi_setWheels(-100, 100);
+                        timer_waitMillis(3200);
+                        oi_setWheels(0, 0);
+
+                     }
+                        float angleToObject = run_autonomous_scan(sensor_data,&scan);
+                        flag_monitor(sensor_data);
+
+                              //If the scan is cancelled, we don't want it to calculate angles
+                              if(angleToObject == -1){
+                                cancel_scan_flag = 0;
+                                break;
+                            }
+
+
+                     distance = get_largest_dist();
+                     lcd_printf("Dist: %lf", distance);
+
+
+
+
+                     if (angleToObject < 90){ //turn to the right
+                         angleToObject =  90 - angleToObject; //Angle to move the bot right
+                         turn_clockwise(sensor_data, angleToObject);
+                     }
+                     else if (angleToObject > 90) {
+                         angleToObject = angleToObject - 90;  //Angle to move the bot left
+                         turn_counterclockwise(sensor_data, angleToObject);
+                     }
+                     angleToObject = 0;
+
+
+
+                     move_forward(sensor_data, distance * 8, 1);
+
+                     lcd_clear();
+                     lcd_printf("dist %f : %f", distance, sum);
+                     oi_play_song(0);
+                     timer_waitMillis(3000);
+                     state = MANUAL;
+
+                     break;
+
+        }
+
+    }
+
+        else if (state == MANUAL){
+
+            while (state == MANUAL){
+                lcd_printf("we are in manual");
+
+                oi_update(sensor_data);
+                int mode = 0;
+
+//                GET COLOR VALUES
+//                int csR  = sensor_data->cliffRightSignal;
+//                int csFR = sensor_data->cliffFrontRightSignal;
+//                int csL  = sensor_data->cliffLeftSignal;
+//                int csFL = sensor_data->cliffFrontLeftSignal;
+//                lcd_printf("L:%d FL:%d  FR:%d  R:%d ", csL, csFL, csFR, csR);
+
+//                lcd_printf("we are in manual");
+                //MANUAL MODE DRIVING
+                flag_monitor(sensor_data);
+                //index = 0;  // Set index to the beginning of the command buffer
+                my_data = cyBot_getByte(); // Get first byte of the command from the Client
+
+                switch (my_data)
+                {
+                case 'w':
+
+                    mode = move_forward(sensor_data, 40, 0); //10cm
+                    if(mode == 1){
+                        state = AUTO;
+                        break;
+
+                    }
+                    break;
+                case 'a':
+                    turn_counterclockwise(sensor_data, 45);
+                    break;
+                case 's':
+                    move_backward(sensor_data, 40); //10cm
+                    break;
+                case 'd':
+                    turn_clockwise(sensor_data, 45);
+                    break;
+                case 'f':
+                    oi_setWheels(0, 0);
+                    break;
+                case 'u':
+                    //update
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }
+
+    oi_free(sensor_data); // do this once at end of main()
+
+}
+
+void flag_monitor(oi_t *sensor_data){
+    //start scan
+
+    cyBOT_Scan_t scan;
+
+    //also includes cancel scan flag check within scan.c
+    if (ping_scan_flag == 1){
+        lcd_clear();
+        lcd_puts("Scanning...");
+        float x = run_autonomous_scan(sensor_data, &scan);
+        lcd_clear();
+
+        ping_scan_flag = 0;
+        cancel_scan_flag = 0;
+    }
+
+    //ir scan
+    if (ir_scan_flag == 1){
+            lcd_clear();
+            lcd_puts("IR scan...");
+            ir_scan();
+            ir_scan_flag = 0;
+            cancel_scan_flag = 0;
+        }
+
+    //force stop
+    if (force_stop_flag == 1){
+        lcd_clear();
+        lcd_puts("FORCE STOP.");
+        //CANCEL ALL ACTIONS
+        oi_setWheels(0, 0); //stops wheels
+        //flags that cancel scans, sensors, etc are set to 1 in uart-interrupt.c
+        //under force_stop_flag which is == 'c' if statement
+        //which causes a force stop
+        //then they must be reset here:
+        ping_scan_flag = 0;
+        cancel_scan_flag = 0;
+        force_stop_flag = 0;
+    }
+
+    //toggle driving modes
+    if (auto_flag == 1){
+        lcd_clear();
+        lcd_puts("SWITCHED TO:              AUTO MODE.");
+
+        state = AUTO;
+        auto_flag = 0;
+    }
+    if (manual_flag == 1){
+        lcd_clear();
+        lcd_puts("SWITCHED TO:              MANUAL MODE.");
+
+        state = MANUAL;
+        manual_flag = 0;
+    }
+
+}
---
+++ cyBot_Functionality/scan.c
@@ -0,0 +1,187 @@
+#include "Timer.h"
+#include "lcd.h"
+#include <stdint.h>
+#include "movement.h"
+#include "open_interface.h"
+#include "cyBot_uart.h"
+#include "cyBot_Scan.h"
+#include "uart-interrupt.h"
+#include "scan.h"
+#include "adc.h"
+#include <stdbool.h>
+#define AVERAGE_COUNT 4
+
+int ir_vals[] = {3758, 3308, 2305, 1852, 1621, 1463, 1312, 1207, 1125, 1069, 1044, 1016, 931, 925, 900, 832, 822, 799, 775, 757};
+int dist_cm[] = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100};
+int current_ir_vals[16];
+int largest_Distance;
+float largest_Width;
+
+
+int ir_scan(){
+    char data[50]; //for the data
+    char buffer[10]; //for lcd data print
+
+    int x = 0;
+    int i = 0;
+    double avg_ir;
+    int distance_cm;
+
+    while (x < 16){
+        uint32_t IR_val;
+        //ADC scan data using built adc
+        timer_waitMillis(1000); //helps improve ADC values due to decrease in noise
+        IR_val = adc_conversion();
+        current_ir_vals[x] = IR_val;
+
+        if (cancel_scan_flag == 1) {
+            lcd_clear();
+            lcd_puts("CANCELLED SCAN.");
+            return 0;
+        }
+
+        sprintf(data, "%d\r\n", IR_val);   //print out data
+        char* dataPosition = data;
+        while(*dataPosition !='\0'){
+
+
+
+            cyBot_sendByte(*dataPosition++);
+        }
+
+        x++;
+
+    }
+
+    for (; i < 19; i++){
+         avg_ir += current_ir_vals[i];
+    }
+    avg_ir = avg_ir / 16;
+    distance_cm = obj_distance(avg_ir);
+
+
+
+    return distance_cm;
+}
+
+int obj_distance(double avg_ir) {
+    int k = 0;
+    for (; k < 18; k++){
+        if (avg_ir > ir_vals[k]){
+            return dist_cm[k] - 2; //if the avg ir value is less than
+            //the standard plot then we have found a match
+            //and we can use the matching index of the dist_cm to get the distance in cm
+            //and subtract two to adjust for it being a bit shorter than the plot point
+        }
+    }
+    return 105;
+}
+
+
+float run_autonomous_scan(oi_t *sensor_data, cyBOT_Scan_t *scan)
+{
+    // 1) Base case: if we've reached the recursion limit, stop
+
+    // 2) Clear any leftover objects
+    obj storedObj[10] = {0};
+
+    // Variables for scanning
+    double distanceReadings[AVERAGE_COUNT] = {0};
+    int index = 0;
+    int currentObj = 0;
+    int objCount = 0;
+    int firstDegree = 0, lastDegree = 0;
+    double firstDistance = 0;
+
+    // 3) Perform the scan loop, same as your 'm' command
+    timer_waitMillis(500); // let servo settle
+    int angle;
+    for (angle = 0; angle <= 180; angle += 2) {
+
+        cyBOT_Scan(angle, scan);
+
+        // Rolling average distance
+        distanceReadings[index] = scan->sound_dist;
+        index = (index + 1) % AVERAGE_COUNT;
+
+        double avgDistance = 0;
+        int j;
+        for (j = 0; j < AVERAGE_COUNT; j++) {
+            avgDistance += distanceReadings[j];
+        }
+        avgDistance /= AVERAGE_COUNT;
+
+        // Print data
+        char data_line[64];
+        sprintf(data_line, "%d\t%.2f\t%d\n",
+                angle, scan->sound_dist, scan->IR_raw_val, avgDistance);
+        uart_sendStr(data_line);
+
+        // Simple IR-based detection: IR >= 900 & distance < 60
+        if (scan->IR_raw_val >= 900 && currentObj == 0 && avgDistance < 60.0) {
+            objCount++;
+            currentObj = 1;
+            firstDegree = angle;
+            firstDistance = avgDistance;
+        }
+        else if (scan->IR_raw_val < 900 && currentObj == 1) {
+            lastDegree = angle;
+            currentObj = 0;
+
+            // Record object
+            int degObj = (firstDegree + lastDegree) / 2;
+            int radialWidth = lastDegree - firstDegree;
+
+            char msg[80];
+           // sprintf(msg, "OBJECT #%d: angle=%d dist=%.2f width=%d\r\n",
+            //        objCount, degObj, firstDistance, radialWidth);
+            //uart_sendStr(msg);
+
+            storedObj[objCount - 1].objNum  = objCount;
+            storedObj[objCount - 1].width  = radialWidth;
+            storedObj[objCount - 1].angle  = degObj;
+            storedObj[objCount - 1].distance = firstDistance;
+
+            double size = ((double) radialWidth / 360.0) * (2.0 * M_PI) * firstDistance;
+            storedObj[objCount - 1].TRUEWIDTH = size;
+
+            //sprintf(msg, "SIZE: %.2f\r\n", size);
+            //uart_sendStr(msg);
+        }
+    } // end for
+
+    // 4) If no objects, done
+    if (objCount == 0) {
+        //uart_sendStr("No objects detected.\r\n");
+        uart_sendStr("END\n");
+        return 0;
+    }
+
+    // 5) Pick smallest object
+    obj largestObj = storedObj[0];
+    double largestWidth = storedObj[0].TRUEWIDTH;
+
+    int i;
+    for(i = 1; i < objCount; i++){
+        if(storedObj[i].TRUEWIDTH > largestWidth){
+            largestWidth = storedObj[i].TRUEWIDTH;
+            largestObj = storedObj[i];
+        }
+    }
+
+    cyBOT_Scan(largestObj.angle, scan); // point to the largest width
+    largest_Distance = largestObj.distance;
+    largest_Width = largestObj.width;
+    lcd_printf("Width; %d",largestObj.width );
+    uart_sendStr("END\n");
+
+    return largestObj.angle;
+
+
+
+    }
+
+
+
+int get_largest_dist() {return largest_Distance;}
+float get_largest_width() {return largest_Width;}
